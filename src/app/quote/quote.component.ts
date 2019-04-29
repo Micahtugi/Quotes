8cbaf1a55540a6034e615fd4116a948b11e21e50
Micah <micahkimathi@gmail.com>
2019-04-30 01:26:31 +0300

    Inputted the submit form function

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,6 +9,7 @@ export class QuoteComponent implements OnInit {
   quotes = [
     new Quote(1,'Tugi','Champion from the beginning'),
   ]
+
   deleteQuote(isComplete,index){
     if (isComplete){
         let toDelete=confirm(`Are you sure you want to delete ${this.quotes[index].name}`)
@@ -22,6 +23,12 @@ export class QuoteComponent implements OnInit {
 
   toogleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id=quoteLength+1;
+    quote.completeDate = new Date (quote.completeDate)
+    this.quotes.push(quote)
   }
 
   constructor() { }
