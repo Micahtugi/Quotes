01dc67998112e318948f2843a266765eabe07f52
Micah <micahkimathi@gmail.com>
2019-04-30 09:33:17 +0300

    Finall commit

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -11,7 +11,6 @@ export class QuoteFormComponent implements OnInit {
   @Output() addQuote=new EventEmitter<Quote>();
   submitQuote(){
     this.addQuote.emit(this.newQuote);
-    this.newQuote=new Quote(0,"","");
   }
 
   constructor() { }
